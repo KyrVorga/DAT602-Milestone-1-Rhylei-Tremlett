a21c1703a9722e0dd2a47934cfd455316b0158f2
Rhylei <Rhylei@aureate.dev>
2023-03-28 21:44:08 +1300

    completed registration page

---
+++ StageOne/LoginAndRegistrationDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace StageOne
+{
+    internal class LoginAndRegistrationDAO : DatabaseAccessObject
+    {
+
+        public String RegisterUser(String username_param, String email_param, String password_param)
+        {
+            List<MySqlParameter> procedure_params = new List<MySqlParameter>();
+
+            MySqlParameter username = new("@username", MySqlDbType.VarChar, 50)
+            {
+                Value = username_param
+            };
+            procedure_params.Add(username);
+
+            MySqlParameter email = new("@email", MySqlDbType.VarChar, 100)
+            {
+                Value = email_param
+            };
+            procedure_params.Add(email);
+
+            MySqlParameter password = new("@password", MySqlDbType.VarChar, 50)
+            {
+                Value = password_param
+            };
+            procedure_params.Add(password);
+
+            DataSet auth_result = MySqlHelper.ExecuteDataset(DatabaseAccessObject.MySqlConnection, "call CreateAccount(@username, @email, @password)", procedure_params.ToArray());
+
+
+            return auth_result.Tables[0].Rows[0]["MESSAGE"].ToString();
+        }
+    }
+}
---
+++ StageOne/Registration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace StageOne
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void register_button_Click(object sender, EventArgs e)
+        {
+            string username = username_input.Text;
+            string password = password_input.Text;
+            string email = email_input.Text;
+
+            LoginAndRegistrationDAO db_connection = new();
+
+
+            if (db_connection.RegisterUser(username, password, email) == "Error")
+            {
+                MessageBox.Show("Account creation failed, please try again.");
+            }
+            else
+            {
+                MessageBox.Show("Account created successfully.");
+                this.Hide();
+                //Game game = new Game(this);
+                //game.Show();
+            }
+        }
+    }
+}
